790070a4d5a38accbe0dc3ab196da2d1875231ba
ShuttlerBad <dileepasudara@gmail.com>
2024-10-02 12:06:33 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,21 @@
 require('dotenv').config()
 
 const express = require('express')
+const workoutRoutes = require('./routes/workout')
 
 //express app
 const app = express()
 
 //middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 }) 
 
 //routes
-app.get('/', (req, res) => {
-    res.json({mssg:'Welcome to the app'})
-})
+app.use('/api/workouts', workoutRoutes)
 
 //listen port request
 app.listen(process.env.PORT, ()=>{
